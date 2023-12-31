8d06232548093313ba0f9fd270a2f7c455c96f0d
Seenivasan-Developer <148607571+Seenivasan-Developer@users.noreply.github.com>
2023-12-31 13:11:55 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 var num =10;
-document.getElementById("counter").innerHTML=num;
+document.querySelector("h1").innerHTML=num;
 setTimeout(() => {
     num=num-1;
-    document.getElementById("counter").innerHTML=num;
+    document.querySelector("h1").innerHTML=num;
     setTimeout(() => {
         num=num-1;
-        document.getElementById("counter").innerHTML=num;
+        document.querySelector("h1").innerHTML=num;
         setTimeout(() => {
             num=num-1;
-            document.getElementById("counter").innerHTML=num;
+            document.querySelector("h1").innerHTML=num;
             setTimeout(() => {
                 num=num-1;
-                document.getElementById("counter").innerHTML=num;
+                document.querySelector("h1").innerHTML=num;
                 setTimeout(() => {
                     num=num-1;
-                    document.getElementById("counter").innerHTML=num;
+                    document.querySelector("h1").innerHTML=num;
                     setTimeout(() => {
                         num=num-1;
-                        document.getElementById("counter").innerHTML=num;
+                        document.querySelector("h1").innerHTML=num;
                         setTimeout(() => {
                             num=num-1;
-                            document.getElementById("counter").innerHTML=num;
+                            document.querySelector("h1").innerHTML=num;
                             setTimeout(() => {
                                 num=num-1;
-                                document.getElementById("counter").innerHTML=num;
+                                document.querySelector("h1").innerHTML=num;
                                 setTimeout(() => {
                                     num=num-1;
-                                    document.getElementById("counter").innerHTML=num;
+                                    document.querySelector("h1").innerHTML=num;
                                     setTimeout(() => {
-                                        document.getElementById("counter").innerHTML="<div class='main'>HAPPY</div><div><span class='main1'> INDE</span><span class='main3'> PEND</span><span class='main1'>ENCE</span><div><div class='main2'>  DAY</div>";
+                                        document.querySelector("h1").innerHTML="<div class='main'>HAPPY</div><div><span class='main1'> INDE</span><span class='main3'>PEND</span><span class='main1'>ENCE</span><div><div class='main2'> DAY</div>";
                                     }, 1000);
                                 }, 1000);
                              }, 1000);
