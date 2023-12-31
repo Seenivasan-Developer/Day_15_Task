e484a4025d10eb95bf21fb1c41741ac8a587afe8
Seenivasan-Developer <148607571+Seenivasan-Developer@users.noreply.github.com>
2023-12-31 13:00:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+var num =10;
+document.getElementById("counter").innerHTML=num;
+setTimeout(() => {
+    num=num-1;
+    document.getElementById("counter").innerHTML=num;
+    setTimeout(() => {
+        num=num-1;
+        document.getElementById("counter").innerHTML=num;
+        setTimeout(() => {
+            num=num-1;
+            document.getElementById("counter").innerHTML=num;
+            setTimeout(() => {
+                num=num-1;
+                document.getElementById("counter").innerHTML=num;
+                setTimeout(() => {
+                    num=num-1;
+                    document.getElementById("counter").innerHTML=num;
+                    setTimeout(() => {
+                        num=num-1;
+                        document.getElementById("counter").innerHTML=num;
+                        setTimeout(() => {
+                            num=num-1;
+                            document.getElementById("counter").innerHTML=num;
+                            setTimeout(() => {
+                                num=num-1;
+                                document.getElementById("counter").innerHTML=num;
+                                setTimeout(() => {
+                                    num=num-1;
+                                    document.getElementById("counter").innerHTML=num;
+                                    setTimeout(() => {
+                                        document.getElementById("counter").innerHTML="<div class='main'>HAPPY</div><div><span class='main1'> INDE</span><span class='main3'> PEND</span><span class='main1'>ENCE</span><div><div class='main2'>  DAY</div>";
+                                    }, 1000);
+                                }, 1000);
+                             }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
